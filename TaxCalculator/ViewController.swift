64e3f70a5ad7634bb15e0607f236da525deb709c
Asol-Nusu <nusuvalieva_a@auca.kg>
2021-01-21 12:37:28 +0600

    Initial Commit

---
+++ TaxCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxCalculator
+//
+//  Created by Asol on 1/21/21.
+//  Copyright © 2021 Asol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
